d3782a3bb9d5a5cc6ea35619d9330c8ebeccc98d
ideepe98-ctrl <ideepe98@gmail.com>
2026-09-05 07:19:39 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,31 @@
 #include <mfapi.h>
 #include <mfvirtualcamera.h>
 
+#pragma comment(lib, "mfsensorgroup.lib")
+
 int main()
 {
-    return 0;
+    HRESULT hr = MFStartup(MF_VERSION);
+    if (FAILED(hr))
+        return 1;
+
+    IMFVirtualCamera* camera = nullptr;
+
+    hr = MFCreateVirtualCamera(
+        MFVirtualCameraType_SoftwareCameraSource,
+        MFVirtualCameraLifetime_Session,
+        MFVirtualCameraAccess_CurrentUser,
+        L"DeepeCam",
+        L"{8A5E5C91-4D7B-4E35-9F3B-1C8A9B7D21F4}",
+        nullptr,
+        0,
+        &camera
+    );
+
+    if (camera)
+        camera->Release();
+
+    MFShutdown();
+
+    return SUCCEEDED(hr) ? 0 : 2;
 }
