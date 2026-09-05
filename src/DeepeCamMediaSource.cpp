9a6f496eefbab5c694b85b525367e27c99af0ee8
ideepe98-ctrl <ideepe98@gmail.com>
2026-09-05 07:44:26 +0530

    Update DeepeCamMediaSource.cpp

--- src/DeepeCamMediaSource.cpp
+++ src/DeepeCamMediaSource.cpp
@@ -1,1 +1,10 @@
+#include <windows.h>
 
+extern "C" __declspec(dllexport)
+BOOL APIENTRY DllMain(
+    HMODULE hModule,
+    DWORD ul_reason_for_call,
+    LPVOID lpReserved)
+{
+    return TRUE;
+}
