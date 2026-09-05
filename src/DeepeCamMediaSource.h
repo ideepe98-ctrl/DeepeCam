677bc35e2e6f15dea553982a598d6fd868e6d7e7
ideepe98-ctrl <ideepe98@gmail.com>
2026-09-05 08:50:16 +0530

    Update DeepeCamMediaSource.h

--- src/DeepeCamMediaSource.h
+++ src/DeepeCamMediaSource.h
@@ -4,13 +4,33 @@
 #include <mfapi.h>
 #include <mfidl.h>
 #include <mfobjects.h>
-#include <mftransform.h>
 
-class DeepeCamMediaStream
+class DeepeCamMediaStream : public IMFMediaStream
 {
 public:
     DeepeCamMediaStream();
-    ~DeepeCamMediaStream();
+    virtual ~DeepeCamMediaStream();
 
-    HRESULT Initialize();
+    // IUnknown
+    STDMETHODIMP QueryInterface(REFIID riid, void** ppv) override;
+    STDMETHODIMP_(ULONG) AddRef() override;
+    STDMETHODIMP_(ULONG) Release() override;
+
+    // IMFMediaEventGenerator
+    STDMETHODIMP GetEvent(DWORD flags, IMFMediaEvent** event) override;
+    STDMETHODIMP BeginGetEvent(IMFAsyncCallback* callback, IUnknown* state) override;
+    STDMETHODIMP EndGetEvent(IMFAsyncResult* result, IMFMediaEvent** event) override;
+    STDMETHODIMP QueueEvent(
+        MediaEventType met,
+        REFGUID extendedType,
+        HRESULT status,
+        const PROPVARIANT* value) override;
+
+    // IMFMediaStream
+    STDMETHODIMP GetMediaSource(IMFMediaSource** source) override;
+    STDMETHODIMP GetStreamDescriptor(IMFStreamDescriptor** descriptor) override;
+    STDMETHODIMP RequestSample(IUnknown* token) override;
+
+private:
+    long m_refCount = 1;
 };
