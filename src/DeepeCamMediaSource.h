b1743ba73dbae6341481ede34c9f969ad67b0cd6
ideepe98-ctrl <ideepe98@gmail.com>
2026-09-05 08:47:52 +0530

    Update DeepeCamMediaSource.h

--- src/DeepeCamMediaSource.h
+++ src/DeepeCamMediaSource.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#include <windows.h>
+#include <mfapi.h>
+#include <mfidl.h>
+#include <mfobjects.h>
+#include <mftransform.h>
+
+class DeepeCamMediaStream
+{
+public:
+    DeepeCamMediaStream();
+    ~DeepeCamMediaStream();
+
+    HRESULT Initialize();
+};
