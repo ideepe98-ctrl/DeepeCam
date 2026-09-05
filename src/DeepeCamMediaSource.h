8e1288f89aa4b50c9b4e5c42e66c4616dc102bad
ideepe98-ctrl <ideepe98@gmail.com>
2026-09-05 09:10:18 +0530

    Update DeepeCamMediaSource.h

--- src/DeepeCamMediaSource.h
+++ src/DeepeCamMediaSource.h
@@ -1,40 +1,11 @@
 #pragma once
 
-#include <windows.h>
-#include <mfapi.h>
-#include <mfidl.h>
-#include <mfobjects.h>
+#include "DeepeCamMediaSource.h"
 
-class DeepeCamMediaSource : public IMFMediaSource
+class DeepeCamFactory
 {
 public:
-    DeepeCamMediaSource();
-    virtual ~DeepeCamMediaSource();
-
-    STDMETHODIMP QueryInterface(REFIID riid, void** ppv) override;
-    STDMETHODIMP_(ULONG) AddRef() override;
-    STDMETHODIMP_(ULONG) Release() override;
-
-    STDMETHODIMP GetEvent(DWORD flags, IMFMediaEvent** event) override;
-    STDMETHODIMP BeginGetEvent(IMFAsyncCallback* callback, IUnknown* state) override;
-    STDMETHODIMP EndGetEvent(IMFAsyncResult* result, IMFMediaEvent** event) override;
-    STDMETHODIMP QueueEvent(
-        MediaEventType met,
-        REFGUID extendedType,
-        HRESULT status,
-        const PROPVARIANT* value) override;
-
-    STDMETHODIMP GetCharacteristics(DWORD* characteristics) override;
-    STDMETHODIMP CreatePresentationDescriptor(
-        IMFPresentationDescriptor** descriptor) override;
-    STDMETHODIMP Start(
-        IMFPresentationDescriptor* descriptor,
-        const GUID* timeFormat,
-        const PROPVARIANT* startPosition) override;
-    STDMETHODIMP Stop() override;
-    STDMETHODIMP Pause() override;
-    STDMETHODIMP Shutdown() override;
-
-private:
-    long m_refCount = 1;
+    static HRESULT CreateInstance(
+        REFIID riid,
+        void** ppv);
 };
